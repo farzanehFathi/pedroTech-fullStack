77efdbab00ba9d67951ff5508505d1e1dc59847e
farzaneh <farzaneh.fth@gmail.com>
2023-07-23 13:54:44 +0300

    axios and bootstrap installed

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default function Weather() {
-  <div className="weather">
-    Weather app
-    <form>
-      <input
-        type="search"
-        placeholder="please enter a city .."
-        autoComplete="off"
-      />
-      <input type="submit" value="search" />
-    </form>
-  </div>;
+  return (
+    <div className="weather">
+      <form>
+        <input
+          type="search"
+          placeholder="please enter a city .."
+          autoComplete="off"
+        />
+        <input type="submit" value="search" />
+      </form>
+    </div>
+  );
 }
