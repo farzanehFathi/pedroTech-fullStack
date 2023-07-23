70c3fcbc05cebdeafbe6e057b99b0d078920afdb
farzaneh <farzaneh.fth@gmail.com>
2023-07-23 13:48:57 +0300

    added Weather component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <footer>
+        <a href="https://github.com/farzanehFathi/react-weather-app">
+          Open Source
+        </a>{" "}
+        code by Farzaneh
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Weather() {
+  <div className="weather">
+    Weather app
+    <form>
+      <input
+        type="search"
+        placeholder="please enter a city .."
+        autoComplete="off"
+      />
+      <input type="submit" value="search" />
+    </form>
+  </div>;
+}
