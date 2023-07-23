cde34bdbbe91c216d57a877610c3823d7bb7e7bc
farzaneh <farzaneh.fth@gmail.com>
2023-07-23 14:07:49 +0300

    html structure created

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,32 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="weather">
+    <div className="Weather">
       <form>
         <input
           type="search"
           placeholder="please enter a city .."
           autoComplete="off"
         />
-        <input type="submit" value="search" />
+        <input type="submit" value="search" className="btn btn-primary" />
       </form>
+      <div>
+        <h1>Tehran</h1>
+        <ul>
+          <li>Sunday, 14:04</li>
+          <li>Clear Sky</li>
+        </ul>
+      </div>
+      <div>
+        <div>Icon</div> <div>38</div> <div>°C</div>
+      </div>
+      <div>
+        <ul>
+          <li>Humidiy: 45%</li> <li>Wind: 5 km/h</li>
+        </ul>
+      </div>
     </div>
   );
 }
