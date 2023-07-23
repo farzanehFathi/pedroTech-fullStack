94e14b2c8dac294aec68f2e4d92d15d53bd5b6e0
farzaneh <farzaneh.fth@gmail.com>
2023-07-23 14:47:30 +0300

    styling is done

--- src/Weather.js
+++ src/Weather.js
@@ -4,27 +4,43 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <input
-          type="search"
-          placeholder="please enter a city .."
-          autoComplete="off"
-        />
-        <input type="submit" value="search" className="btn btn-primary" />
+      <form className="row gx-0 mb-3">
+        <div className="row gx-0">
+          <div className="col-9">
+            <input
+              className="form-control h-100 w-75"
+              type="search"
+              placeholder="please enter a city .."
+              autoComplete="off"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              className="btn btn-primary w-100"
+              type="submit"
+              value="search"
+            />
+          </div>
+        </div>
       </form>
-      <div>
+
+      <div className="mb-3">
         <h1>Tehran</h1>
         <ul>
           <li>Sunday, 14:04</li>
           <li>Clear Sky</li>
         </ul>
       </div>
-      <div>
-        <div>Icon</div> <div>38</div> <div>°C</div>
-      </div>
-      <div>
-        <ul>
-          <li>Humidiy: 45%</li> <li>Wind: 5 km/h</li>
+      <div className="row gx-0">
+        <ul className="col-6 weather-summary">
+          <li className="icon">Icon</li>
+          <li className="temp">38</li>
+          <li className="unit">°C</li>
+        </ul>
+        <ul className="col-6">
+          <li>Humidiy: 45%</li>
+          <li>Wind: 5 km/h</li>
         </ul>
       </div>
     </div>
