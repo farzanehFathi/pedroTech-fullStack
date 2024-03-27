46044a54a311a60857548eb4f6916d2abb8d6d4a
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-03-27 11:24:27 +0330

    reset the data for new project practice

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import "./App.css";
 import Weather from "./Weather";
 
 function App() {
-  return (
-    <div className="App">
-      <Weather defaultCity="oslo" />
-      <footer>
-        <a href="https://github.com/farzanehFathi/react-weather-app">
-          Open Source
-        </a>{" "}
-        code by Farzaneh
-      </footer>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
